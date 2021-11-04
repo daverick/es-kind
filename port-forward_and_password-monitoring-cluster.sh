14bcdbf12fc1ca9cea3782a77830b33757e41e6c
daverick <david.ricordel@elastic.co>
2021-11-04 19:09:55 +0100

    fix kb conf and adjust port forwarding

--- port-forward_and_password-monitoring-cluster.sh
+++ port-forward_and_password-monitoring-cluster.sh
@@ -6,7 +6,7 @@ PASSWORD=$(kubectl get secret monitoring-cluster-es-elastic-user -o=jsonpath='{.
 
 echo "Password for monitoring elasticsearch is: $PASSWORD"
 
-kubectl port-forward service/monitoring-kibana-kb-http 5602  > /dev/null 2>&1 &
+kubectl port-forward service/monitoring-kibana-kb-http 5602:5601  > /dev/null 2>&1 &
 
 echo "----"
 echo "Connect to Kibana at: \"https://localhost:5602\""
